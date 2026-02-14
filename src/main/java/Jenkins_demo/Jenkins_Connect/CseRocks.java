34a6b7b390a4aeb134afd6c5f56e090857ca269b
Nithin <attilinithin@gmail.com>
2026-02-14 15:08:59 +0530

    Added Add and sub methods and added test cases for them

---
+++ src/main/java/Jenkins_demo/Jenkins_Connect/CseRocks.java
@@ -0,0 +1,10 @@
+package Jenkins_demo.Jenkins_Connect;
+
+public class CseRocks {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+}
---
+++ src/test/java/Jenkins_demo/Jenkins_Connect/AppTest2.java
@@ -0,0 +1,19 @@
+package Jenkins_demo.Jenkins_Connect;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest2 {
+
+	CseRocks cse = new CseRocks();
+	@Test
+	void testAdd() {
+		assertEquals(10, cse.add(5, 5));
+	}
+	@Test
+	void testSub() {
+		assertEquals(0, cse.sub(5, 5));
+	}
+
+}
