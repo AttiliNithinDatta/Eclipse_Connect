93fe59aaa2d6cd5c606e0143424e1812a493384b
AttiliNithinDatta <23mh1a05l8@acoe.edu.in>
2026-02-14 14:53:09 +0530

    Changes in the app.java

--- src/main/java/Jenkins_demo/Jenkins_Connect/App.java
+++ src/main/java/Jenkins_demo/Jenkins_Connect/App.java
@@ -5,6 +5,6 @@ package Jenkins_demo.Jenkins_Connect;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Jenkins");
     }
 }
